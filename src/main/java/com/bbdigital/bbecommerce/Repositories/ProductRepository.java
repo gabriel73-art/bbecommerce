6ceed7a2d27b3efffc07f0fc73a102b2e950a531
Gabriel Monteagudo <gmonteagudomartinez96@gmail.com>
2023-06-01 15:53:27 -0400

    Eliminando cunsultas sobrantes

--- src/main/java/com/bbdigital/bbecommerce/Repositories/ProductRepository.java
+++ src/main/java/com/bbdigital/bbecommerce/Repositories/ProductRepository.java
@@ -26,15 +26,7 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     long countByDescriptionContaining(String description);
     long countByDetailsContaining(String details);
 
-
-
-    @Query("select distinct p from Product p where Upper(p.name) like %:param% or Upper(p.category) like %:cat_param% ")
-    Page<Product> findByProductName(@Param("param") String name,@Param("cat_param") String category, Pageable pageable);
-
-    @Query("select distinct p from Product p where Upper(p.name) like %:param% or Upper(p.category) like %:cat_param% or p.price=:price_param")
-    List<Product> getQuantityProductName(@Param("param") String name,@Param("cat_param") String category,
-                                    @Param("price_param") Double price);
-
+    
     @Query("select distinct p from Product p where p.stock=0")
     List<Product> getNoExistenceProducts();
 
